d28da514321387144970ad56e7837993515058d2
グリアンドロ <guriandoro@gmail.com>
2022-07-27 17:27:45 -0300

    Modified ardupilot's start coords and qgroundcontrol mission plan
    mission.plan can be used from QGroundControl to upload to drone

--- scripts/gdscf/start_sitl.sh
+++ scripts/gdscf/start_sitl.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Start coord: -33.9559789,-55.6752125,584,353
+
+mkdir -p $HOME/.config/ardupilot/
+echo "Uruguay=-34.9559789,-54.6752125,584,353" > $HOME/.config/ardupilot/locations.txt
+
 cd ~/ardupilot/ArduCopter/
-sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+sim_vehicle.py -v ArduCopter -f gazebo-iris -L Uruguay --console
